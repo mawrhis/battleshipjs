bd1ba0de76454a1b5b21f298c396ae2fd89f27e6
mawrhis <jirkaprihoda@gmail.com>
2017-08-29 07:56:04 +0200

    page 61

--- battleship.js
+++ battleship.js
@@ -12,5 +12,21 @@ while (isSunk == false) {
 		alert("Please enter a valid cell number!")
 	} else {
 		guesses = guesses + 1;
+
+		if (guess == location1 || guess == location2 || guess == location3) {
+			hits = hits + 1;
+
+			alert("HIT!");
+
+			if (hits == 3) {
+				isSunk = true;
+				alert ("You sank my battleship!");
+			}
+		} else {
+			alert ("MISS!")
+		}
 	}
 }
+
+var stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your shooting accuracy was " + (3/guesses);
+alert(stats);
