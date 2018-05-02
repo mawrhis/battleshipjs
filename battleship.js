5c3989169ee33721d194a04279766f6d5feff344
mawrhis <jirkaprihoda@gmail.com>
2018-05-02 11:00:09 +0200

    view + model

--- battleship.js
+++ battleship.js
@@ -1,8 +1,60 @@
 var view = {
   displayMessage: function(msg) {
+    var messageArea = document.getElementById("messageArea");
+    messageArea.innerHTML = msg;
   },
   displayHit: function(location) {
+    var cell = document.getElementById(location);
+    cell.setAttribute("class", "hit");
   },
   displayMiss: function(location) {
+    var cell = document.getElementById(location);
+    cell.setAttribute("class", "miss");
   }
 };
+
+
+
+
+var model = {
+  boardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shipsSunk: 0,
+
+  ships: [{ locations: ["10", "20", "30"], hits: ["","",""] },
+          { locations: ["32", "33", "34"], hits: ["","",""] },
+          { locations: ["63", "64", "65"], hits: ["","",""] }],
+
+  fire: function(guess) {
+    for (var i = 0; i < this.numShips; i++) {
+      var ship =this.ships[i];
+      var index = ship.locations.indexOf(guess); //indexOf(arg) goes through array and finds location (index) of arg
+      if (index >= 0) {
+        //we have a hit
+        ship.hits[index] = "hit";
+        if (this.isSunk(ship)) {
+          this.shipsSunk++;
+        }
+        return true;
+      }
+    }
+    return false
+  },
+  isSunk: function(ship) {
+    for (var i = 0; i < this.shipLength; i++)  {
+      if(ship.hits[i] !== "hit") {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
+view.displayMessage("Tap tap, is this thing on?");
