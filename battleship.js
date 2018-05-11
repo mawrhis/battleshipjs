7fab26992e65d5638490f1e7207a23ca0d6aa803
mawrhis <jirkaprihoda@gmail.com>
2018-05-11 14:46:46 +0200

    var controller

--- battleship.js
+++ battleship.js
@@ -26,7 +26,7 @@ var model = {
           { locations: ["13", "14", "15"], hits: ["","",""] },
           { locations: ["26", "36", "46"], hits: ["","",""] }],
 
-  fire: function(guess) {
+  fire: function(guess) { //method
     for (var i = 0; i < this.numShips; i++) {
       var ship = this.ships[i];
       var index = ship.locations.indexOf(guess); //indexOf(arg) goes through array and finds location (index) of arg
@@ -46,7 +46,7 @@ var model = {
     view.displayMessage("You missed.")
     return false
   },
-  isSunk: function(ship) {
+  isSunk: function(ship) { //method
     for (var i = 0; i < this.shipLength; i++)  {
       if(ship.hits[i] !== "hit") {
         return false;
@@ -55,3 +55,35 @@ var model = {
     return true;
   }
 };
+
+var controller = {
+  guesses: 0,
+
+  processGuess: function(guess) {
+    function parseGuess (guess) {
+      var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+      if (guess === null || guess.length !== 2) {
+        alert("Oops, please enter a letter and a number on the board.");
+      } else {
+        firstChar = guess.charAt(0) //grab the first character of guess
+        var row = alphabet.indexOf(firstChar);
+        var column = guess.chartAt(1);
+
+        if (isNan(row) || inNaN(column)) {
+          alert("Oops, that isn't on the board.");
+        } else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
+          alert ("Oops, that's off the board!");
+        }
+      }
+    }
+  }
+}
+
+
+// TODO/docs
+
+// what is null?
+// what is NaN - not a number
+// || returns true if either operand is true
+// array - pole
